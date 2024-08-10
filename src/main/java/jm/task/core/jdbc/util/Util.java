bd6e7fd6b9215aeba97a157b2324806c23752860
muromtsev <t4k.org@mail.ru>
2024-08-10 14:22:09 +0300

    Update util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,6 +15,15 @@ public class Util {
     private static final String USERNAME_KEY = "db.username";
     private static final String PASSWORD_KEY = "db.password";
     private static final String URL_KEY = "db.url";
+
+    public Session getConnection() {
+        Configuration cfg = new Configuration().addAnnotatedClass(User.class);
+
+        try (SessionFactory sessionFactory = cfg.buildSessionFactory()) {
+            return sessionFactory.getCurrentSession();
+        }
+    }
+
 
     public Connection open() {
         try {
